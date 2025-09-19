d71f1a1229b94679163b416c5eed5080d6ae6ca6
NessDan <NessDan@users.noreply.github.com>
2025-09-19 02:19:47 -0400

    Allow scaling and unscaling. Retry adding button if fails.

--- content.js
+++ content.js
@@ -1,6 +1,18 @@
-window.addEventListener("load", () => {
+const checkAndAddControls = () => {
   const controls = document.querySelector(".ytp-right-controls");
   if (controls) {
+    const style = document.createElement("style");
+    style.innerHTML = `
+        .yt-aspect-scaled.html5-video-container {
+          height: 100% !important;
+        }
+        .yt-aspect-scaled .html5-main-video {
+          height: 100% !important;
+          top: 0 !important;
+        }
+      `;
+    document.head.appendChild(style);
+
     const button = document.createElement("button");
     button.innerText = "Scale";
     button.classList.add("ytp-button");
@@ -8,21 +20,27 @@ window.addEventListener("load", () => {
     button.style.width = "auto";
     button.style.marginRight = "6px";
 
-    const btnListener = button.addEventListener("click", () => {
-      const style = document.createElement("style");
-      style.innerHTML = `
-        .html5-video-container {
-          height: 100% !important;
-        }
-        .html5-main-video {
-          height: 100% !important;
-          top: 0 !important;
-        }
-      `;
-      document.head.appendChild(style);
-      document.removeEventListener("click", btnListener);
+    button.addEventListener("click", () => {
+      if (button.innerText === "Scale") {
+        button.innerText = "Unscale";
+        document
+          .querySelector(".html5-video-container")
+          .classList.add("yt-aspect-scaled");
+      } else {
+        button.innerText = "Scale";
+        document
+          .querySelector(".html5-video-container")
+          .classList.remove("yt-aspect-scaled");
+      }
     });
 
     controls.insertBefore(button, controls.firstChild);
+  } else {
+    // Retry if controls aren't there.
+    setTimeout(() => {
+      checkAndAddControls();
+    }, 100);
   }
-});
+};
+
+window.addEventListener("load", checkAndAddControls);
